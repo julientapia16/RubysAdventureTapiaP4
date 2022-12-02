580ef8507a54ea80f67b84283bbfba91d083ad04
julientapia16 <116380837+julientapia16@users.noreply.github.com>
2022-12-02 11:46:36 -0800

    Added colliders
    added colliders and fixed up the physics

--- RubysAdventureTapia/Assets/Scripts/RubyController.cs
+++ RubysAdventureTapia/Assets/Scripts/RubyController.cs
@@ -4,28 +4,30 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
-        //creatte 2 variables to use the unity built in axis
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        //Create my movement vector variable
-        Vector2 position = transform.position;
-
-        //Create the movement of my character
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //Set the position
-        transform.position = position;
-
+        rigidbody2d.MovePosition(position);
     }
 }
+   
