a042212527949165b3b6e235d17564dacf7e8600
julientapia16 <116380837+julientapia16@users.noreply.github.com>
2022-12-06 11:42:53 -0800

    Added enemies
    I added an enemy and started setting up it's movements and damage

---
+++ RubysAdventureTapia/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+}
